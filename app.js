392df4afd79d18c7c179e5aa77a0eb8bff738f0a
Tushar Mistry <mistry.tushar98@gmail.com>
2020-12-13 21:48:46 +0530

    Added config.env and views and some npm scrits

--- app.js
+++ app.js
@@ -5,16 +5,20 @@ const path = require("path");
 // Custom Module
 // Third Party Modules
 const express = require("express");
+const dotenv = require("dotenv");
 
+dotenv.config({ path: "./config.env" });
 // Create Server
 const app = express();
-const port = process.env.PORT || 3000; // Port Number for the website
 
 const USERS = path.join(__dirname, "data", "blogs.json");
 const userData = JSON.parse(fs.readFileSync(USERS, "utf-8"));
 
+app.set("view engine", "ejs");
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.status(200).send("<h1>Welcome Page</h1>");
+  res.status(200).render("home");
 });
 
 app.get("/blogs", (req, res) => {
@@ -25,27 +29,27 @@ app.get("/blogs", (req, res) => {
   });
   res.status(200).json({
     message: "Sucessful",
-    data: data,
+    data: data
   });
 });
 
 app.get("/blogs/:id", (req, res) => {
   let data = userData.find((user) => {
-    return (user.id == req.params.id);
+    return user.id == req.params.id;
   });
   if (data) {
     res.status(200).json({
       message: "Succesful",
-      data: data,
+      data:  data
     });
   } else {
     res.status(404).json({
       message: "Failed",
-      status: "User Not Found",
+      status: "Blog Not Found",
     });
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on http://localhost:${process.env.PORT}`);
 });
